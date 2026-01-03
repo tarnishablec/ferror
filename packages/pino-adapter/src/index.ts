232450ca26bf8511b3f7372406aa43b5e4e7a0b4
tarnishablec <tarnishablec@outlook.com>
2026-01-03 16:24:33 +0800

    chore(packages): rename pino-serializer to pino-adapter and update references

--- packages/pino-adapter/src/index.ts
+++ packages/pino-adapter/src/index.ts
@@ -2,13 +2,8 @@
  * Copyright 2019-Present tarnishablec. All Rights Reserved.
  */
 
-import {
-    codeOf,
-    isDefinedError,
-    payloadOf,
-    type ThatError,
-} from "@thaterror/core";
-import pino, { type LoggerOptions, type SerializedError } from "pino";
+import {codeOf, isDefinedError, payloadOf, type ThatError,} from "@thaterror/core";
+import pino, {type LoggerOptions, type SerializedError} from "pino";
 
 export const thaterrorSerializer = (e: ThatError): SerializedError => {
     const pinoErr = pino.stdSerializers.errWithCause(e);
@@ -25,7 +20,7 @@ export const thaterrorHooks: LoggerOptions["hooks"] = {
         const err = args[0];
         if (err && isDefinedError(err)) {
             return method.apply(this, [
-                { thaterror: err },
+                {thaterror: err},
                 args[1],
                 ...args.slice(2),
             ]);
